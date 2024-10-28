ddfb25db5d6ec5483da55c398f32acbed9b19652
Junyo <130440930+SSub-jun@users.noreply.github.com>
2024-10-28 16:18:09 +0900

    feat: AnimeQuote 스위프트 데이터 모델 생성
    Anime > Character > AnimeQuote 순의 계층을 가짐.
    각각은 Anime : Character = Character : AnimeQuote = 1:N 의 관계에 있음.
    
    세세한 변수는 주석을 확인하면 됨.
    relationship의 deleterule 은
    - cascade : 해당 상위 개념 삭제 시, 하위 모두 삭제됨.
    - nullify : Anime에 속한 Character 가 모두 삭제되더라도 상위 개념은 삭제되지 않음.

---
+++ Pepperoni/Pepperoni/DataStructure/AnimeQuotes.swift
@@ -0,0 +1,88 @@
+//
+//  AnimeQuotes.swift
+//  Pepperoni
+//
+//  Created by 변준섭 on 10/28/24.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+class Anime {
+    @Attribute(.unique) var id: UUID
+    var title: String // 애니메이션 제목
+    var season: String // 시즌명
+    @Relationship(deleteRule: .cascade) var characters: [Character] // Character와 1:N 관계
+
+    init(id: UUID = UUID(), title: String, season: String, characters: [Character] = []) {
+        self.id = id
+        self.title = title
+        self.season = season
+        self.characters = characters
+    }
+}
+
+@Model
+class Character {
+    @Attribute(.unique) var id: UUID
+    var name: String // 캐릭터 이름
+    var favorite: Bool // 즐겨찾기 여부
+    @Relationship(deleteRule: .cascade) var quotes: [AnimeQuote] // AnimeQuote와 1:N 관계
+    var image: Data? // 사용자가 수정 가능한 캐릭터 이미지
+
+    // Character가 속한 Anime을 나타내기 위한 관계
+    @Relationship(deleteRule: .nullify, inverse: \Anime.characters) var anime: Anime?
+
+    init(id: UUID = UUID(), name: String, favorite: Bool, quotes: [AnimeQuote] = [], image: Data? = nil, anime: Anime? = nil) {
+        self.id = id
+        self.name = name
+        self.favorite = favorite
+        self.quotes = quotes
+        self.image = image
+        self.anime = anime
+    }
+}
+
+@Model
+class AnimeQuote {
+    @Attribute(.unique) var id: UUID
+    var japanese: [String] // 명대사 일본어 배열
+    var pronunciation: [String] // 사용자가 수정 가능한 발음 배열
+    var korean: [String] // 한국어 번역 배열
+    var evaluation: Evaluation // 사용자가 수정 가능한 평가 데이터
+
+    // AnimeQuote가 속한 Character를 나타내기 위한 관계
+    @Relationship(deleteRule: .nullify, inverse: \Character.quotes) var character: Character?
+
+    init(id: UUID = UUID(), japanese: [String], pronunciation: [String], korean: [String], evaluation: Evaluation, character: Character? = nil) {
+        self.id = id
+        self.japanese = japanese
+        self.pronunciation = pronunciation
+        self.korean = korean
+        self.evaluation = evaluation
+        self.character = character
+    }
+}
+
+struct Evaluation: Codable {
+    // pronunciation : 발음, intonation : 억양, speed : 속도
+    // 각각은 점수(Score), 통과여부(Pass) 로 이루어짐
+    var id: UUID
+    var pronunciationScore: Double
+    var pronunciationPass: Bool
+    var intonationScore: Double
+    var intonationPass: Bool
+    var speedScore: Double
+    var speedPass: Bool
+
+    init(id: UUID=UUID(), pronunciationScore: Double, pronunciationPass: Bool, intonationScore: Double, intonationPass: Bool, speedScore: Double, speedPass: Bool) {
+        self.id = id
+        self.pronunciationScore = pronunciationScore
+        self.pronunciationPass = pronunciationPass
+        self.intonationScore = intonationScore
+        self.intonationPass = intonationPass
+        self.speedScore = speedScore
+        self.speedPass = speedPass
+    }
+}
