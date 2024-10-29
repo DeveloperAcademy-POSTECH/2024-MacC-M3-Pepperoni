52b55aa7a5bec9f2aca17d29ff63d54db603abe8
won <mosiccan@gmail.com>
2024-10-29 23:25:20 +0900

    feat: #3 발음 체크 기능 구현

--- Pepperoni/Pepperoni/STTView.swift
+++ Pepperoni/Pepperoni/STTView.swift
@@ -7,23 +7,69 @@
 
 import SwiftUI
 
+// MARK: - 예시 뷰
+// STT, 발음 체크 사용예시 뷰 입니다.
+// 참고만 하세요
+
 struct STTView: View {
     @StateObject private var sttManager = STTManager()
+    
+    let meanings = ["오랜시간", "빌어먹게", "신세를", "졌습니다"]
+    let words = ["長い間", "くそ", "お世話に", "なりました"]
+    let pronunciations = ["나가이아이다", "쿠소", "오세와니", "나리마시타"]
     
     var body: some View {
         VStack(spacing: 20) {
             Text("Japanese Speech-to-Text")
                 .font(.title)
             
-            Text("")
+            // 의미
+            HStack {
+                ForEach(meanings, id: \.self) { meaning in
+                    Text(meaning)
+                        .frame(maxWidth: .infinity)
+                }
+            }
             
-            // sttManager의 recognizedText를 사용하여 텍스트 표시
+            // 일본어 단어
+            HStack {
+                ForEach(words, id: \.self) { word in
+                    Text(word)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            // 발음
+            HStack {
+                ForEach(pronunciations, id: \.self) { pronunciation in
+                    Text("\(pronunciation)")
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            // 발음 체크
+            HStack {
+                ForEach(0..<words.count, id: \.self) { index in
+                    if sttManager.recognizedText.contains(words[index]) {
+                        Text("정확함")
+                            .foregroundColor(.green)
+                            .frame(maxWidth: .infinity)
+                    } else {
+                        Text("다름")
+                            .foregroundColor(.red)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+            }
+            
+            // STT 결과 텍스트 표시
             Text(sttManager.recognizedText)
                 .padding()
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .background(Color.gray.opacity(0.1))
                 .cornerRadius(10)
             
+            // 녹음 버튼
             Button(action: {
                 if sttManager.isRecording {
                     sttManager.stopRecording()
@@ -37,6 +83,7 @@ struct STTView: View {
                     .background(sttManager.isRecording ? Color.red : Color.blue)
                     .cornerRadius(10)
             }
+            
         }
         .padding()
     }
