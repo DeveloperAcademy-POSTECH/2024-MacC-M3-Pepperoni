8765d008ddd52f7b0baaf793595f975a5e853d73
HyunJaeyeon <bomul0109@gmail.com>
2024-10-30 16:37:49 +0900

    chore: #15-HomeView 추가
    - 애니 목록 버튼 -> AnimeList로 넘어감

--- Pepperoni/Pepperoni/View/HomeView.swift
+++ Pepperoni/Pepperoni/View/HomeView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HomeView: View {
     
     var body: some View {
         VStack {
-            Button("검색") {
-                Router.shared.navigate(to: .search)
+            HStack {
+                Text("anisentence")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.ppBlue)
+                Spacer()
+            }
+            .frame(height: 70)
+            
+            Group{
+                Image("Roni")
+                
+                Button("애니 목록") {
+                    Router.shared.navigate(to: .animeList)
+                }
+                .frame(width: 357, height: 60)
+                .foregroundStyle(.white)
+                .background(.ppBlue)
+                .cornerRadius(16)
+            }
+            
+            Group{
+                Rectangle()
+                    .frame(width: 360, height: 180)
+                    .cornerRadius(16)
+                    .foregroundStyle(.ppBlue)
+                
+                Button("최애의 아이") {
+                    Router.shared.navigate(to: .learning(id: "21"))
+                }
+                .frame(width: 180, height: 60)
+                .foregroundStyle(.white)
+                .background(.ppBlue)
+                .cornerRadius(16)
             }
         }
-        .navigationTitle("Home")
+        .padding()
+
     }
 }
 
