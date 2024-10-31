e34cb1b128d83cafc26cde0d45db4b9835bc1ecf
Junyo <130440930+SSub-jun@users.noreply.github.com>
2024-10-31 21:14:03 +0900

    feat: #31 ResultView 완성
    - Evaluation 데이터 활용한 뷰 그리기
    - 조건 만족 시 YoutubeView 표시
    - 간단한 애니메이션 첨가
    
    * 추가로 채점 로직 들어가야 함.

--- Pepperoni/Pepperoni/View/Learning/ResultView.swift
+++ Pepperoni/Pepperoni/View/Learning/ResultView.swift
@@ -10,95 +10,156 @@ import SwiftUI
 struct ResultView: View {
     let quote: AnimeQuote
     
-    var dummieQuote: AnimeQuote = AnimeQuote(japanese: ["才能は", "開花させる", "もの", "センスは", "磨く", "もの"], pronunciation: ["사이노우와", "카이카사세루", "모노", "센스와", "미가쿠", "모노"], korean: ["재능은", "발휘하는", "것", "센스는", "연마하는", "것"], evaluation: Evaluation(pronunciationScore: 78.0, pronunciationPass: false, intonationScore: 93, intonationPass: false, speedScore: 95.0, speedPass: false), timemark: [2.0, 2.5, 3.3, 5.0, 5.4, 6.0], voicingTime: 0.0, audiofile: "HIQ001.m4a", youtubeID: "", youtubeStartTime: 0, youtubeEndTime: 10)
+    @State private var isScoreAnimated: Bool = false // ScoreBar 애니메이션 완료 여부
+    @State private var isStarAnimated: Bool = false // Star 애니메이션 완료 여부
+    @State private var showCongratulation: Bool = false // 축하 메시지
+    
+    private var totalScore: Int {
+        Int(quote.evaluation.pronunciationScore + quote.evaluation.intonationScore + quote.evaluation.speedScore)
+    }
+    
+    private var totalPass: Bool{
+        quote.evaluation.pronunciationPass && quote.evaluation.intonationPass && quote.evaluation.speedPass
+    }
     
     var body: some View {
-        ZStack {
-             Color.gray
-                 .ignoresSafeArea()
-             VStack {
-                 HStack {
-                     Spacer()
-                     Button(action: {
-                         // 닫기 액션
-                     }) {
-                         Image(systemName: "xmark")
-                             .foregroundColor(.white)
-                             .font(.system(size: 24))
-                     }
-                 }
-                 .padding()
-                 
-                 Spacer()
-                 
-                 VStack(spacing: 20) {
-                     HStack(spacing: 10) {
-                         // 별 3개
-                         ForEach(0..<3) { index in
-                             Image(systemName: index == 1 ? "star" : "star.fill")
-                                 .resizable()
-                                 .frame(width:100, height:100)
-                                 .foregroundColor(index == 1 ? Color.blue : Color.white)
-                         }
-                     }
-                     
-                     HStack(spacing: 30) {
-                         // 발음, 높낮이, 스피드의 퍼센트 바
-                         ScoreBar(title: "발음", score: 30)
-                         ScoreBar(title: "높낮이", score: 120, highlighted: true)
-                         ScoreBar(title: "스피드", score: 50)
-                     }
-                     
-                     Text("3888점")
-                         .font(.largeTitle)
-                         .bold()
-                         .foregroundColor(Color.blue)
-                     
-                     Button(action: {
-                         // 재시도 액션
-                     }) {
-                         Image(systemName: "arrow.counterclockwise.circle.fill")
-                             .font(.system(size: 40))
-                             .foregroundColor(Color.gray)
-                     }
-                 }
-                 .padding()
-                 .background(Color.blue.opacity(0.2))
-                 .cornerRadius(20)
-                 .padding(.horizontal, 40)
-                 
-                 Spacer()
-             }
-         }
+        ScrollView{
+            if showCongratulation && totalPass {
+                VStack(spacing:0){
+                    Rectangle()
+                        .frame(height:65)
+                        .overlay{
+                            Text("Congratulate!")
+                                .foregroundStyle(.ppBlue)
+                                .bold()
+                        }
+                    YouTubePlayerView(videoID: "CGREMa2xFBk", startTime: 20, endTime:25)
+                        .frame(height: 218)
+                        .padding(.bottom, 30)
+                }
+            }
+            
+            Text("\(totalScore) 점")
+                .font(.system(size: 40))
+                .bold()
+            
+            ZStack{
+                VStack{
+                    Spacer()
+                    RoundedRectangle(cornerRadius: 48)
+                        .frame(height:511)
+                        .padding(.horizontal, 20)
+                }
+                
+                VStack{
+                    //MARK: - 별 3개
+                    HStack(spacing:-30){
+                        Star(isPassed: quote.evaluation.pronunciationPass, size: 112)
+                        Star(isPassed: quote.evaluation.intonationPass, size: 127)
+                            .offset(y:-30)
+                        Star(isPassed: quote.evaluation.speedPass, size: 112)
+                    }.padding(.bottom, 18)
+                    
+                    //MARK: - 막대바 및 점수
+                    HStack(spacing:12){
+                        ScoreBar(title: "발음", score: quote.evaluation.pronunciationScore)
+                        ScoreBar(title: "높낮이", score: quote.evaluation.intonationScore)
+                        ScoreBar(title: "스피드", score: quote.evaluation.speedScore)
+                    }
+                    
+                    Image(systemName: "arrow.counterclockwise.circle.fill")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.ppBlue, .blue, .gray)
+                        .font(.system(size: 50))
+                        .padding(.top, 10)
+                    
+                    
+                }
+            }.frame(height:604)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // ScoreBar 애니메이션 후 Star 애니메이션
+                isScoreAnimated = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // Star 애니메이션 후 Congratulation 표시
+                    isStarAnimated = true
+                    
+                    if totalPass {
+                        withAnimation(.easeIn(duration: 0.5)) {
+                            showCongratulation = true
+                        }
+                    }
+                }
+            }
+        }
+        
     }
 }
 
+struct Star: View{
+    
+    @State private var filled: Bool = false // 애니메이션용 상태 변수
+    
+    var isPassed: Bool
+    var size: CGFloat
+    
+    var body: some View{
+        Image(systemName: "star.fill")
+            .resizable()
+            .foregroundStyle(isPassed ? .ppBlue : .gray)
+            .frame(width: size, height: size)
+            .overlay{
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundStyle(filled ? (isPassed ? .blue : .white) : .clear) // 초기에는 색상이 투명하게 설정
+                    .frame(width: size*0.85, height: size*0.85)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // ScoreBar 애니메이션 이후 지연
+                    withAnimation(.easeIn(duration: 0.5)) {
+                        filled = true // 애니메이션이 시작되며 색상이 채워짐
+                    }
+                }
+            }
+    }
+}
+
+
 struct ScoreBar: View {
+    
+    @State private var animatedScore: Double = 0 // 애니메이션용 변수 추가
+    
     var title: String
-    var score: Int
+    var score: Double
     var highlighted: Bool = false
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(highlighted ? Color.blue : Color.gray, lineWidth: 2)
-                .frame(width: 50, height: 150)
-                .overlay(
-                    Text("\(score)%")
-                        .font(.caption)
-                        .bold()
-                        .foregroundColor(.black)
-                        .padding(.bottom, 5),
-                    alignment: .bottom
-                )
-            
+        VStack{
+            ZStack(alignment:.bottom){
+                RoundedRectangle(cornerRadius:16)
+                    .frame(width:80, height:288)
+                    .foregroundStyle(.gray)
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(width: 80, height: 288 * animatedScore / 100)
+                    .foregroundStyle(score == 100.0 ? .blue : .ppBlue)
+                Text("\(Int(score))%")
+                    .font(.system(size: 16, weight: .bold))
+                    .padding(.bottom, 19)
+            }
+            .onAppear {
+                // 애니메이션이 뷰가 나타날 때 발생
+                withAnimation(.easeOut(duration: 1.5)) {
+                    animatedScore = score
+                }
+            }
             Text(title)
-                .font(.caption)
-                .foregroundColor(.black)
+                .foregroundStyle(.white)
+                .bold()
+                .padding(.top, 10)
         }
     }
 }
 
-//#Preview {
-//    ResultView(quote: AnimeQuote(japanese: ["才能は", "開花させる", "もの", "センスは", "磨く", "もの"], pronunciation: ["사이노우와", "카이카사세루", "모노", "센스와", "미가쿠", "모노"], korean: ["재능은", "발휘하는", "것", "센스는", "연마하는", "것"], evaluation: Evaluation(pronunciationScore: 78.0, pronunciationPass: false, intonationScore: 93, intonationPass: false, speedScore: 95.0, speedPass: false), timemark: [2.0, 2.5, 3.3, 5.0, 5.4, 6.0], audiofile: "HIQ001.m4a"))
-//}
+#Preview {
+    ResultView(quote: AnimeQuote(japanese: ["才能は", "開花させる", "もの", "センスは", "磨く", "もの"], pronunciation: ["사이노우와", "카이카사세루", "모노", "센스와", "미가쿠", "모노"], korean: ["재능은", "발휘하는", "것", "센스는", "연마하는", "것"], evaluation: Evaluation(pronunciationScore: 78.0, pronunciationPass: true, intonationScore: 93, intonationPass: true, speedScore: 95.0, speedPass: true), timemark: [2.0, 2.5, 3.3, 5.0, 5.4, 6.0], voicingTime: 0.0, audiofile: "HIQ001.m4a", youtubeID: "", youtubeStartTime: 0, youtubeEndTime: 10))
+}
