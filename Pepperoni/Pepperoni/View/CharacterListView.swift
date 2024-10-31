eb63ca25e5e1093a60b5aa70517a8197d5a76b10
HyunJaeyeon <bomul0109@gmail.com>
2024-10-31 11:19:15 +0900

    feat: #23-CharacterListView 중간 구현

--- Pepperoni/Pepperoni/View/CharacterListView.swift
+++ Pepperoni/Pepperoni/View/CharacterListView.swift
@@ -9,24 +9,74 @@ import SwiftUI
 
 struct CharacterListView: View {
     let anime: Anime
+    
+    var dummieAnime : Anime = Anime(title: "원피스", characters:  [
+        Character(name: "몽키 디 루피", favorite: true),
+        Character(name: "토니토니 초파", favorite: true),
+        Character(name: "롤로노아 조로", favorite: false),
+        Character(name: "니코 로빈", favorite: false),
+        Character(name: "상디", favorite: false)
+    ])
 
     var body: some View {
         VStack {
-            Text(anime.title)
-                .font(.largeTitle)
-                .padding()
-            
-            List(anime.characters, id: \.id) { character in
-                Button {
-                    Router.shared.navigate(to: .characterDetail(character: character))
-                } label: {
-                    Text(character.name)
+            VStack{
+                HStack{
+                    Spacer()
+                    
+                    Text(Image(systemName: "pin.square.fill"))
+                        .foregroundStyle(.white)
+                        .font(.title2)
+                }
+                
+                HStack{
+                    Text(dummieAnime.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .padding()
+                    
+                    Spacer()
                 }
             }
-
-            if anime.favorite {
-                Text("⭐️")
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.gray)
+            
+            List(dummieAnime.characters, id: \.id) { character in
+                Button {
+                    Router.shared.navigate(to: .characterList(anime: anime))
+                } label: {
+                    HStack {
+                        Circle()
+                            .frame(width: 40, height: 40)
+                        Text(character.name)
+                        
+                        Spacer()
+                        
+                        Text("3 / 10")
+                    }
+                    .padding()
+                    .overlay(
+                        // favorite이 true일 때 하트 아이콘 표시
+                        character.favorite ?
+                        Image(systemName: "heart.fill")
+                            .foregroundStyle(.red)
+                            .font(.title3)
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 12)
+                            .offset(x: -10, y: 0) : nil,
+                        alignment: .topLeading
+                    )
+                }
+                .listRowSeparator(.hidden)
+                .frame(maxWidth: .infinity)
+                .frame(height: 68)
+                .background(.gray)
+                .cornerRadius(20)
+                .padding(-2)
             }
+            .listStyle(PlainListStyle())
+            .scrollContentBackground(.hidden)
 
             Spacer()
         }
