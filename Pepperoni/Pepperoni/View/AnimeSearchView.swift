f7bee1080039883062e5daf8667f1fe48c1b2c9c
HyunJaeyeon <bomul0109@gmail.com>
2024-11-01 01:25:32 +0900

    feat/#41-더미 삭제 및 데이터 연결

--- Pepperoni/Pepperoni/View/AnimeSearchView.swift
+++ Pepperoni/Pepperoni/View/AnimeSearchView.swift
@@ -14,54 +14,8 @@ struct AnimeSearchView: View {
     @Environment(\.presentationMode) var presentationMode
     
     private var filteredAnimes: [Anime] {
-        animeArray.filter { !$0.title.isEmpty && searchText.isEmpty == false && $0.title.contains(searchText) }
+        allAnimes.filter { !$0.title.isEmpty && searchText.isEmpty == false && $0.title.contains(searchText) }
     }
-    
-    // 임시 더미 배열
-    let animeArray: [Anime] = [
-        Anime(
-            title: "하이큐",
-            characters: [
-                Character(name: "오이카와 토오루", favorite: false)
-            ],
-            favorite: true
-        ),
-        Anime(
-            title: "하이볼",
-            characters: [
-                Character(name: "오이카와 토오루", favorite: false)
-            ],
-            favorite: true
-        ),
-        Anime(
-            title: "하하하",
-            characters: [
-                Character(name: "오이카와 토오루", favorite: false)
-            ],
-            favorite: true
-        ),
-        Anime(
-            title: "원피스",
-            characters: [
-                Character(name: "빈스모크 상디", favorite: false)
-            ],
-            favorite: true
-        ),
-        Anime(
-            title: "최애의 아이",
-            characters: [
-                Character(name: "호시노 아이", favorite: false)
-            ],
-            favorite: false
-        ),
-        Anime(
-            title: "주술회전",
-            characters: [
-                Character(name: "고죠 사토루", favorite: false)
-            ],
-            favorite: false
-        )
-    ]
 
     var body: some View {
         VStack {
--- Pepperoni/Pepperoni/View/CharacterListView.swift
+++ Pepperoni/Pepperoni/View/CharacterListView.swift
@@ -9,128 +9,6 @@ import SwiftUI
 
 struct CharacterListView: View {
     let anime: Anime
-    
-    var dummieAnime: Anime = Anime(
-        title: "원피스",
-        characters: [
-            Character(
-                name: "몽키 디 루피",
-                favorite: true,
-                quotes: [
-                    AnimeQuote(
-                        japanese: ["才能は", "開花させる", "もの", "センスは", "磨く", "もの"],
-                        pronunciation: ["사이노우와", "카이카사세루", "모노", "센스와", "미가쿠", "모노"],
-                        korean: ["재능은", "발휘하는", "것", "센스는", "연마하는", "것"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [2.0, 2.5, 3.3, 5.0, 5.4, 6.0],
-                        audiofile: "HIQ001.m4a"
-                    ),
-                    AnimeQuote(
-                        japanese: ["長い間", "くそ", "お世話に", "なりました"],
-                        pronunciation: ["나가이아이다", "쿠소", "오세와니", "나리마시타"],
-                        korean: ["오랜시간", "빌어먹게", "신세를", "졌습니다"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [1.9, 3.0, 3.9, 4.6],
-                        audiofile: "ONP001.m4a"
-                    ),
-                    AnimeQuote(
-                        japanese: ["長い間", "くそ", "お世話に", "なりました"],
-                        pronunciation: ["나가이아이다", "쿠소", "오세와니", "나리마시타"],
-                        korean: ["오랜시간", "빌어먹게", "신세를", "졌습니다"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [1.9, 3.0, 3.9, 4.6],
-                        audiofile: "ONP001.m4a"
-                    ),
-                    AnimeQuote(
-                        japanese: ["長い間", "くそ", "お世話に", "なりました"],
-                        pronunciation: ["나가이아이다", "쿠소", "오세와니", "나리마시타"],
-                        korean: ["오랜시간", "빌어먹게", "신세를", "졌습니다"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [1.9, 3.0, 3.9, 4.6],
-                        audiofile: "ONP001.m4a"
-                    )
-                ],
-                completedQuotes: 2
-            ),
-            Character(
-                name: "토니토니 초파",
-                favorite: true,
-                quotes: [
-                    AnimeQuote(
-                        japanese: ["長い間", "くそ", "お世話に", "なりました"],
-                        pronunciation: ["나가이아이다", "쿠소", "오세와니", "나리마시타"],
-                        korean: ["오랜시간", "빌어먹게", "신세를", "졌습니다"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [1.9, 3.0, 3.9, 4.6],
-                        audiofile: "ONP001.m4a"
-                    ),
-                    AnimeQuote(
-                        japanese: ["長い間", "くそ", "お世話に", "なりました"],
-                        pronunciation: ["나가이아이다", "쿠소", "오세와니", "나리마시타"],
-                        korean: ["오랜시간", "빌어먹게", "신세를", "졌습니다"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [1.9, 3.0, 3.9, 4.6],
-                        audiofile: "ONP001.m4a"
-                    ),
-                    AnimeQuote(
-                        japanese: ["長い間", "くそ", "お世話に", "なりました"],
-                        pronunciation: ["나가이아이다", "쿠소", "오세와니", "나리마시타"],
-                        korean: ["오랜시간", "빌어먹게", "신세를", "졌습니다"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [1.9, 3.0, 3.9, 4.6],
-                        audiofile: "ONP001.m4a"
-                    )
-                ],
-                completedQuotes: 3
-            ),
-            Character(
-                name: "롤로노아 조로",
-                favorite: false,
-                quotes: [
-                    AnimeQuote(
-                        japanese: ["あんたの", "推しの子に", "なって", "やる"],
-                        pronunciation: ["안타노", "오시노코니", "낫떼", "야루"],
-                        korean: ["너의", "최애의 아이가", "되어", "줄게"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [0.1, 1.0, 2.0, 2.5],
-                        audiofile: "BST001.m4a"
-                    )
-                ],
-                completedQuotes: 1
-            ),
-            Character(
-                name: "니코 로빈",
-                favorite: false,
-                quotes: [
-                    AnimeQuote(
-                        japanese: ["少し", "乱暴", "しようか"],
-                        pronunciation: ["스코시", "란보우", "시요우카"],
-                        korean: ["조금", "난폭", "해볼까?"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [0.1, 1.4, 2.2],
-                        audiofile: "JUS001.m4a"
-                    )
-                ],
-                completedQuotes: 0
-            ),
-            Character(
-                name: "상디",
-                favorite: false,
-                quotes: [
-                    AnimeQuote(
-                        japanese: ["少し", "乱暴", "しようか"],
-                        pronunciation: ["스코시", "란보우", "시요우카"],
-                        korean: ["조금", "난폭", "해볼까?"],
-                        evaluation: Evaluation(pronunciationScore: 100, pronunciationPass: true, intonationScore: 100, intonationPass: true, speedScore: 100, speedPass: true),
-                        timemark: [0.1, 1.4, 2.2],
-                        audiofile: "JUS001.m4a"
-                    )
-                ],
-                completedQuotes: 1
-            )
-        ],
-        favorite: false
-    )
 
     var body: some View {
         VStack {
@@ -141,17 +19,17 @@ struct CharacterListView: View {
                     Button(action: {
                         toggleFavorite() // 즐겨찾기 상태 토글
                     }) {
-                        Image(systemName: dummieAnime.favorite ? "pin.square.fill" : "pin.square")
+                        Image(systemName: anime.favorite ? "pin.square.fill" : "pin.square")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 25, height: 25)
-                            .foregroundColor(dummieAnime.favorite ? .blue : .white)
+                            .foregroundColor(anime.favorite ? .blue : .white)
                     }
                     .padding(.top)
                 }
             
                 HStack{
-                    Text(dummieAnime.title)
+                    Text(anime.title)
                         .fontWeight(.bold)
                         .foregroundStyle(.white)
                         .padding()
@@ -164,7 +42,7 @@ struct CharacterListView: View {
             .background(.gray)
             
             // -MARK: 캐릭터 리스트
-            List(dummieAnime.characters, id: \.id) { character in
+            List(anime.characters, id: \.id) { character in
                 Button {
                     Router.shared.navigate(to: .characterDetail(character: character))
                 } label: {
@@ -190,7 +68,7 @@ struct CharacterListView: View {
     }
     
     private func toggleFavorite() {
-        dummieAnime.favorite.toggle() // favorite 상태를 토글
+        anime.favorite.toggle() // favorite 상태를 토글
     }
 
 }
