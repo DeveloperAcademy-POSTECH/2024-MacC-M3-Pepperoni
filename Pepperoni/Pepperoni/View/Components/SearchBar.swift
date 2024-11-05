0e26e69eda6662f91da318753d1f49c55de30a0c
HyunJaeyeon <bomul0109@gmail.com>
2024-11-05 23:46:08 +0900

    chore: #84-SearchBar에서 cancel 제거
    - 재사용을 위함

--- Pepperoni/Pepperoni/View/Components/SearchBar.swift
+++ Pepperoni/Pepperoni/View/Components/SearchBar.swift
@@ -9,30 +9,20 @@ import SwiftUI
 
 struct SearchBar: View {
     @Binding var searchText: String
-    @Environment(\.presentationMode) var presentationMode // 이전 화면으로 돌아가기 위한 환경 변수
-
+    
     var body: some View {
         HStack {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(.lightGray2)
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.lightGray2)
                 
-                TextField("애니 검색", text: $searchText)
-                    .foregroundColor(.blue)
-                    .padding(.vertical, 10)
-            }
-            .padding(.horizontal)
-            .background(.lightGray1)
-            .cornerRadius(10)
-            
-            Button("Cancel") {
-                searchText = ""
-                presentationMode.wrappedValue.dismiss()
-            }
-            .foregroundColor(.black)
-            .padding(.leading, 8)
+            TextField("애니 검색", text: $searchText)
+                .foregroundColor(.blue)
+                .padding(.vertical, 10)
+                .font(.body)
         }
-        .padding()
+        .padding(.horizontal)
+        .background(.lightGray1)
+        .cornerRadius(6)
     }
 }
 
